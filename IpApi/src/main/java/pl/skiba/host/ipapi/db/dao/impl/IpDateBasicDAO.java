e0fc474f6ca7d103662c38ba7ec8f562c335ae25
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2020-11-21 00:35:19 +0100

    Add save impl

--- IpApi/src/main/java/pl/skiba/host/ipapi/db/dao/impl/IpDateBasicDAO.java
+++ IpApi/src/main/java/pl/skiba/host/ipapi/db/dao/impl/IpDateBasicDAO.java
@@ -2,6 +2,11 @@ package pl.skiba.host.ipapi.db.dao.impl;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import pl.skiba.host.ipapi.db.dao.IpDateDAO;
@@ -9,11 +14,18 @@ import pl.skiba.host.ipapi.db.model.IpDate;
 
 @Repository("IpDateBasicDAO")
 public class IpDateBasicDAO implements IpDateDAO {
+	Logger log = LoggerFactory.getLogger(IpDateBasicDAO.class);
 
 	@Override
 	public void save(IpDate entity) {
-		// TODO Auto-generated method stub
-
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		try (Session session = sessionFactory.openSession();) {
+			session.beginTransaction();
+			session.save(entity);
+			session.getTransaction().commit();
+		} catch (Throwable e) {
+			log.error("Problem executing save: ", e);
+		}
 	}
 
 	@Override
