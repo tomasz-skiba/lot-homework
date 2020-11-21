fa0f61c49fffab6e8201dfb7c69b6d2676a48c1f
Tomasz Skiba <st.skiba.tomasz@gmail.com>
2020-11-21 01:27:04 +0100

    Fix date serialization

--- IpApi/src/main/java/pl/skiba/host/ipapi/controller/dto/IPCountByDayDTO.java
+++ IpApi/src/main/java/pl/skiba/host/ipapi/controller/dto/IPCountByDayDTO.java
@@ -2,21 +2,16 @@ package pl.skiba.host.ipapi.controller.dto;
 
 import java.util.Date;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-
-import org.springframework.format.annotation.DateTimeFormat;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import pl.skiba.host.ipapi.utils.SimpleRegex;
 
 public class IPCountByDayDTO {
-	@NotBlank
-	@DateTimeFormat(pattern = SimpleRegex.DATE_FORMAT)
+	@JsonFormat(pattern = SimpleRegex.DATE_FORMAT)
 	public Date date;
-	@NotBlank
 	public Long count;
 
-	public IPCountByDayDTO(@NotEmpty Date date, @NotEmpty Long count) {
+	public IPCountByDayDTO(Date date, Long count) {
 		super();
 		this.date = date;
 		this.count = count;
