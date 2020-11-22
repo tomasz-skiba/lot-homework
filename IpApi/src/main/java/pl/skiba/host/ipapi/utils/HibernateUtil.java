64c9d11631a696e25bca9eaa631c4b9bd7d1fbcd
Tomasz Skiba <skibielec@gmail.com>
2020-11-22 20:13:59 +0100

    Remove env from properties

--- IpApi/src/main/java/pl/skiba/host/ipapi/utils/HibernateUtil.java
+++ IpApi/src/main/java/pl/skiba/host/ipapi/utils/HibernateUtil.java
@@ -27,8 +27,8 @@ public class HibernateUtil {
 	public static Session getCurrentSession() throws HibernateException {
 		Map<String, String> settings = new HashMap<>();
 		settings.put("hibernate.connection.url", "jdbc:mysql://localhost:3306/lot-maria-db");
-		settings.put("hibernate.connection.username", System.getenv("MYSQL_ROOT_USER"));
-		settings.put("hibernate.connection.password", System.getenv("MYSQL_ROOT_PASSWORD"));
+		settings.put("hibernate.connection.username", "root");
+		settings.put("hibernate.connection.password", "toor");
 		settings.put("connection.driver_class", "com.mysql.jdbc.Driver");
 		settings.put("dialect", "org.hibernate.dialect.MySQL57Dialect");
 		settings.put("hibernate.show_sql", "true");
